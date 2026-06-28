ce83c15f8d44c795b62beafd710dbd3a09a669f6
LeandroAgustinAcevedo <leandroagust@outlook.com.ar>
2026-06-28 20:54:15 -0300

    MODIFY: product.routes.js

--- src/api/routes/product.routes.js
+++ src/api/routes/product.routes.js
@@ -1,30 +1,22 @@
 import { Router } from "express";
 import { validateProduct } from "../middlewares/product.middleware.js";
 import { createProduct, getAllProducts, getProductById, modifyProduct, removeProduct, getProductStockById } from "../controllers/product.controllers.js";
+import { requireLogin } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-
 // GET all products
 router.get("/", getAllProducts);
-
-
 // GET by id
 router.get("/:id", getProductById);
-
-
 // GET product s`Stock by id
 router.get("/:id", getProductStockById);
 
 // POST product
-router.post("/", validateProduct, createProduct);
-
-
+router.post("/", requireLogin, validateProduct, createProduct);
 // UPDATE product
-router.put("/", modifyProduct);
-
-
+router.put("/", requireLogin, validateProduct, modifyProduct);
 // DELETE product
-router.delete("/:id", removeProduct);
+router.delete("/:id", requireLogin, removeProduct);
 
 export default router;
