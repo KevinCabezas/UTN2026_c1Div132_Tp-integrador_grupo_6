41cfdafd0cee6efbdeedbb63f41be7316621159e
KevinCabezas <angelescabesuchi@gmail.com>
2026-06-30 11:39:22 -0300

    Add view customer

--- src/api/routes/view.routes.js
+++ src/api/routes/view.routes.js
@@ -1,12 +1,22 @@
 import { Router } from "express";
 import { join, __dirname } from "../utils/index.js";
-import { indexView, loginView } from "../controllers/view.controllers.js";
+import { indexView, loginView, productsView, listProductsView, customerView } from "../controllers/view.controllers.js";
 
 const router = Router();
 
 // pagina que se mostrara apenas se inicie la app
-router.get("/", loginView);
+router.get("/", indexView);
 
-router.get("/dashboard", indexView);
+// rutas del admin
+router.get("/auth", loginView);
+
+router.get("/dashboard", listProductsView);
+
+
+// rutas del customer
+router.get("/shop", customerView);
+router.get("/shop/products", productsView);
+
+
 
 export default router;
